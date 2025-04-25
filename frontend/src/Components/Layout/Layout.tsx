6bd5650534bc80ee19a0bc60a71ef99feaf953b1
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-04-25 11:31:29 +0200

    added css for header + sidemeny

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -1,53 +1,53 @@
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import React, { useState } from "react";
+import '../../CSS/Layout.css';
 
 const Layout = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    }
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        <>
-            <header>
-                <h1>Pixelhaven</h1>
-                
-                {/*Links for create account and log in.*/}
-                <div className="LogIn-signUp-Links">
-                   <Link to={'/Signup'}>
-                    SIGN UP
-                    </Link>
-                    <Link to={'/Login'}>
-                        LOG IN
-                    </Link> 
+  return (
+    <div className={`Wrapper ${menuOpen ? 'menu-open' : ''}`}>
+      <header>
+        <div className="header-container">
+          <h1 className="Logo-text">Pixelhaven</h1>
+
+          <div className={`right-section ${menuOpen ? "menu-open" : ""}`}>
+            <div className="LogIn-signUp-Links">
+              <Link to={"/Signup"}>SIGN UP</Link>
+              <Link to={"/Login"}>LOG IN</Link>
+            </div>
+
+            <div className="menu-icon">
+              <i
+                className="fa-solid fa-bars menu-icon"
+                onClick={toggleMenu}
+              ></i>
+              {menuOpen && (
+                <div className="side-menu open">
+                  <Link to="/" onClick={() => setMenuOpen(false)}>
+                    DISCOVERY
+                  </Link>
+                  <Link to="/Gametracker" onClick={() => setMenuOpen(false)}>
+                    GAMETRACKER
+                  </Link>
                 </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </header>
 
-                {/*Menu icon*/}
-                <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
-                
-                {menuOpen && (
-                    <nav className="menu">
-                        <Link to="/">
-                            DISCOVERY
-                        </Link>
-                        <Link to="/Gametracker">
-                            GAMETRACKER
-                        </Link>
-                    </nav>
-                )}
+      <main>
+        <Outlet />
+      </main>
 
-            </header>
-
-            <main>
-                <Outlet />
-            </main>
-
-            <footer>
-
-            </footer>
-        </>
-    );
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Layout;
